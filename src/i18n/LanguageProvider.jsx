152a467d15dd725da3a946a3617574065aaddfdf
Nicolas Martinez <nicmart@gmail.com>
2024-06-01 15:55:04 -0700

    make sure correct language displays on initial load

--- src/i18n/LanguageProvider.jsx
+++ src/i18n/LanguageProvider.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { createIntl, createIntlCache, RawIntlProvider } from "react-intl";
-import messages from "./strings.json"; // Import JSON file
+import messages from "./strings.json";
 
 const cache = createIntlCache();
 
-const LanguageContext = React.createContext(); // Create context
+export const LanguageContext = createContext(); // Export LanguageContext
 
 const LanguageProvider = ({ children }) => {
   const [locale, setLocale] = useState("en");
+
+  useEffect(() => {
+    const getLocale = () => {
+      const language = navigator.language.split(/[-_]/)[0];
+      return ["en", "fr"].includes(language) ? language : "en";
+    };
+
+    setLocale(getLocale());
+  }, []);
 
   const intl = createIntl(
     {
@@ -28,4 +37,4 @@ const LanguageProvider = ({ children }) => {
   );
 };
 
-export { LanguageProvider as default, LanguageContext }; // Export LanguageProvider as default
+export default LanguageProvider;
