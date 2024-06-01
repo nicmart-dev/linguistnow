d7ebf8b864aefab9bcea3ff60b83d643465bbff1
Nicolas Martinez <nicmart@gmail.com>
2024-06-01 14:33:15 -0700

    update app.jsx to use react-router for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import "./App.css";
-import Home from "./pages/Home";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
