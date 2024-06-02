e414b2ed306943c785876c8db082ebbb44ac51af
Nicolas Martinez <nicmart@gmail.com>
2024-06-01 17:03:19 -0700

    use cors middleware

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ const app = express();
 require('dotenv').config(); // load environment variables from a .env file into process.env
 const PORT = process.env.PORT || 5000; // Define the port number, use environment variable if available
 
+const cors = require('cors');
+
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(cors()); // allow * / all to access our api. All domains, ips, ports
+
 
 // Routes
 app.get('/', (req, res) => {
