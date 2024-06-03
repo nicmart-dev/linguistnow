470232fee790cf1f42899ed4a9b1390e9e9b506c
Nicolas Martinez <nicmart@gmail.com>
2024-06-02 18:26:43 -0700

    add google client id and secret to server env variable

--- server/server.js
+++ server/server.js
@@ -5,7 +5,13 @@ require('dotenv').config(); // load environment variables from a .env file into 
 const PORT = process.env.PORT || 5000; // Define the port number, use environment variable if available
 
 const cors = require('cors');
+
 const { OAuth2Client } = require('google-auth-library'); // needed to get access token from Google
+const oAuth2Client = new OAuth2Client(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    'postmessage',
+);
 
 const usersController = require('./controllers/usersController');
 
