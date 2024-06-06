e8a463ded5c70003ffb2610d3072bea8808b40e5
Nicolas Martinez <nicmart@gmail.com>
2024-06-06 12:07:37 -0700

    add comments to calendar controller functions

--- server/controllers/calendarController.js
+++ server/controllers/calendarController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 
+/* Check if given user is available against calendars they specified */
 const triggerN8nWorkflow = async (calendarIds, accessToken) => {
     try {
         // URL of N8n webhook per https://docs.n8n.io/integrations/builtin/core-nodes/n8n-nodes-base.webhook/?utm_source=n8n_app&utm_medium=node_settings_modal-credential_link&utm_campaign=n8n-nodes-base.webhook
@@ -17,6 +18,10 @@ const triggerN8nWorkflow = async (calendarIds, accessToken) => {
     }
 };
 
+/* Save selected calendars
+TODO: refactor so calendars are stored in DB, and 
+n8n workflow triger only when PM search for available linguists
+*/
 const saveSelectedCalendars = async (req, res) => {
     const { calendarIds, googleAccessToken } = req.body; // Receive calendar ids list and access token from the front-end
 
