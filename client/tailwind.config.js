dc4a8ff7dd590b4c48bae9df67c09742c1baed2e
Nicolas Martinez <nicmart@gmail.com>
2024-06-08 10:18:18 -0700

    start only setting default font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,28 +5,8 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
     fontFamily: {
       sans: ['Inter var', ...defaultTheme.fontFamily.sans],
     },
-    extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
   },
 }
