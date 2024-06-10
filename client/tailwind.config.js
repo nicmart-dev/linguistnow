075e9bdec9bd920da98aa4a8aeecebb0cf0f13c2
Nicolas Martinez <nicmart@gmail.com>
2024-06-09 17:35:08 -0700

    onfigure tailwind.config.js for TanStack table library

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,21 +8,72 @@ module.exports = {
     fontFamily: {
       sans: ['Inter var', ...defaultTheme.fontFamily.sans],
     },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        google: {
-          'text-gray': '#3c4043',
-          'button-blue': '#1a73e8',
-          'button-blue-hover': '#5195ee',
-          'button-dark': '#202124',
-          'button-dark-hover': '#555658',
-          'button-border-light': '#dadce0',
-          'logo-blue': '#4285f4',
-          'logo-green': '#34a853',
-          'logo-yellow': '#fbbc05',
-          'logo-red': '#ea4335',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
+  plugins: [require("tailwindcss-animate")],
 }
